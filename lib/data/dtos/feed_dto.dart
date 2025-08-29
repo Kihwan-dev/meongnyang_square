bb95d61039aba9235bc288eb358f36f69e0644a9
Kihwan-dev <rlrkf2420@gmail.com>
2025-08-29 20:58:58 +0900

    ✨ Feed Dto 작성

---
+++ lib/data/dtos/feed_dto.dart
@@ -0,0 +1,34 @@
+class Feed {
+  Feed({
+    required this.id,
+    required this.createdAt,
+    required this.tag,
+    required this.content,
+    required this.imagePath,
+  });
+
+  final String? id;
+  final DateTime? createdAt;
+  final String? tag;
+  final String? content;
+  final String? imagePath;
+
+  Feed.fromJson(Map<String, dynamic> map)
+      : this(
+          id: map["id"] ?? "",
+          createdAt: DateTime.parse(map["createdAt"] ?? ""),
+          tag: map["tag"] ?? "",
+          content: map["content"] ?? "",
+          imagePath: map["imagePath"] ?? "",
+        );
+
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+      "createdAt": createdAt?.toIso8601String(),
+      "tag": tag,
+      "content": content,
+      "imagePath": imagePath,
+    };
+  }
+}

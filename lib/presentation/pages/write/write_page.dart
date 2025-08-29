fab073854de73694f61c1a0f969e69ff0a171989
moong414 <moongom414@gmail.com>
2025-08-29 20:03:41 +0900

    💄 comment, write page UI 수정

--- lib/presentation/pages/write/write_page.dart
+++ lib/presentation/pages/write/write_page.dart
@@ -23,98 +23,101 @@ class _WritePageState extends State<WritePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
         backgroundColor: Colors.black,
-        elevation: 0,
-        leading: IconButton(
-          icon: Image.asset('assets/images/icon_back.png',
-          width: 40,
-          height: 40
-          ),
-          onPressed: () => Navigator.of(context).maybePop(),
-        ),
-        centerTitle: true,
-        title: Image.asset('assets/images/logo_s.png',
-        width: 55,
-        height: 55,
-        ),
-        actions: [
-          IconButton(
-            icon: Image.asset('assets/images/icon_pencil.png',
-            width: 30,
-            height: 30),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 20),
-              const Padding(
-                padding: EdgeInsets.only(left: 16.0),
-                child: Text(
-                  'Write',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 20,
-                  ),
-                ),
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          elevation: 0,
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IconButton(
+              icon: Image.asset('assets/images/icon_back.png',
               ),
-              const SizedBox(height: 3),
-              Expanded(
-                child: Column(
-                  children: [
-                    const SizedBox(height: 20),
-                    Center(
-                      child: Container(
-                        width: 140,
-                        height: 140,
-                        decoration: BoxDecoration(
-                          color: Color(0xFF1E1E1E),
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        alignment: Alignment.center,
-                        child: Image.asset(
-                          'assets/images/icon_photo.png',
-                          width: 35,
-                          height: 35,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    Expanded(
-                      child: TagAndContentCard(
-                        tagController: tagController,
-                        contentController: contentController,
-                        maximumLength: maximumLength,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Positioned(
-            left: 24,
-            bottom: 24,
-            child: FloatingActionButton(
-              heroTag: 'trashFloatingActionButton',
-              backgroundColor: const Color(0xFF2C2C2C),
-              onPressed: () {},
-              shape: const CircleBorder(),
-              child: const ImageIcon(
-                AssetImage('assets/images/icon_trash.png'),
-                size: 25,
-                color: Colors.white,
-              ),
+              onPressed: () => Navigator.of(context).maybePop(),
             ),
           ),
-        ],
+          centerTitle: true,
+          title: Image.asset('assets/images/logo_s.png', width: 40, height: 20),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: IconButton(
+                icon: Image.asset('assets/images/icon_pencil.png',),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+        body: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 20),
+                const Padding(
+                  padding: EdgeInsets.only(left: 30),
+                  child: Text(
+                    'Write',
+                    style: TextStyle(
+                      color: Color.fromRGBO(255, 255, 255, .6),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 3),
+                Expanded(
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 20),
+                      Center(
+                        child: Container(
+                          width: 120,
+                          height: 120,
+                          decoration: BoxDecoration(
+                            color: Color(0xFF1E1E1E),
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          alignment: Alignment.center,
+                          child: Image.asset(
+                            'assets/images/icon_photo.png',
+                            width: 35,
+                            height: 35,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 22),
+                      Expanded(
+                        child: TagAndContentCard(
+                          tagController: tagController,
+                          contentController: contentController,
+                          maximumLength: maximumLength,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Positioned(
+              left: 24,
+              bottom: 24,
+              child: FloatingActionButton(
+                heroTag: 'trashFloatingActionButton',
+                backgroundColor: const Color(0xFF2C2C2C),
+                onPressed: () {},
+                shape: const CircleBorder(),
+                child: const ImageIcon(
+                  AssetImage('assets/images/icon_trash.png'),
+                  size: 25,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

1e5ab986c6e53ec4b964e26e1051f5c65a7e11bb
moong414 <moongom414@gmail.com>
2025-09-02 18:33:07 +0900

    ✨ Error페이지 추가

--- lib/presentation/pages/error/error_page.dart
+++ lib/presentation/pages/error/error_page.dart
@@ -3,6 +3,15 @@ import 'package:flutter/material.dart';
 class ErrorPage extends StatelessWidget{
   @override
   Widget build(Object context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+          elevation: 0,
+          centerTitle: true,
+          title: Image.asset('assets/images/logo_s.png', width: 40, height: 20), 
+        ),
+        body: Center(
+          child: Text('Error Page!!'),
+        ),
+    );
   }
 }

053b340ea1637074efad6658ad148fdd4c414117
moong414 <moongom414@gmail.com>
2025-09-02 10:40:00 +0900

    🐛 notification build 내용수정

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,21 +10,21 @@ plugins {
 
 android {
     namespace = "com.example.meongnyang_square"
-    compileSdk = 34
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
-        coreLibraryDesugaringEnabled true
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
-        multiDexEnabled true
+        multiDexEnabled = true
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.meongnyang_square"
         // You can update the following values to match your application needs.
@@ -45,11 +45,15 @@ android {
 }
 
 dependencies {
-  coreLibraryDesugaring 'com.android.tools:desugar_jdk_libs:1.2.2'
-  implementation 'androidx.window:window:1.0.0'
-  implementation 'androidx.window:window-java:1.0.0'
+  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+  implementation ("androidx.window:window:1.0.0")
+  implementation ("androidx.window:window-java:1.0.0")
 }
 
 flutter {
     source = "../.."
+}
+
+kotlin {
+    jvmToolchain(17)
 }
